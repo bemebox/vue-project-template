416aa0f22911071c3962b07dd0a4c131e948afa6
bemebox <bemebox@gmail.com>
2024-11-24 10:10:44 +0000

    improve the 404 page

--- src/components/404NotFound.vue
+++ src/components/404NotFound.vue
@@ -2,8 +2,12 @@
   <div class="py-4 items-center justify-center text-center">
     <font-awesome-icon class="py-4 w-10 h-10" icon="ban" />
     <h1 class="text-6xl mb-4">404</h1>
-    <p class="text-xl">Page Not Found</p>
-    <router-link to="/" class="mt-4 text-blue-500 hover:text-blue-700">Go back to Home</router-link>
+    <p class="text-base">
+      Page not found. Check the address or
+      <router-link to="/dashboard" class="mt-4 text-blue-500 hover:text-blue-700"
+        >go back</router-link
+      >.
+    </p>
   </div>
 </template>
 <script lang="ts">
