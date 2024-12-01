476cfa7af58e210b68f4dac529e261d7730c00d2
bemebox <bemebox@gmail.com>
2024-12-01 09:55:44 +0000

    add template default site url

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/template',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
